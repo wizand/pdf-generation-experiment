3f799133f3561347c9727cd9f3bf13bef11ed0f4
Petteri Loisko <petteri.loisko@gmail.com>
2025-08-22 14:16:38 +0300

    Added a simple authorization mechanism

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# Introduction
 Simple example on how to set up a PDF generation container using weasyprint ( https://weasyprint.org/ ), FLASK ( https://flask.palletsprojects.com/en/stable ) and gunicorn ( https://gunicorn.org/ ) 
+
+# Notes
+Set environmental variable SECRET_TOKEN to a kinda API key, the
+endpoint will ensure that the request has Bearer SECRET_TOKEN as it's
+Authorization header
--- pdfapi.py
+++ pdfapi.py
@@ -1,11 +1,34 @@
 from flask import Flask, request, send_file
 from weasyprint import HTML
 import io
+import os
 
+EXPECTED_TOKEN = os.environ.get("SECRET_TOKEN", "NA")
 app = Flask(__name__)
+
+
+def isAuthorized(request):
+    auth_header = request.headers.get("Authorization")
+    if auth_header is None:
+        return False
+    
+    if not auth_header.startswith("Bearer "):
+        return False
+    
+    token = auth_header.split(" ", 1)[1]
+    if token is None:
+        return False
+    
+    return token == EXPECTED_TOKEN
+
 
 @app.route('/generate-pdf', methods=['POST'])
 def generate_pdf():
+    
+    if ( isAuthorized(request) == False ):
+        return "Unauthorized", 401
+    
+    
     html_content = request.data.decode('utf-8')
     pdf_io = io.BytesIO()
     HTML(string=html_content).write_pdf(pdf_io)
@@ -13,5 +36,8 @@ def generate_pdf():
     return send_file(pdf_io, mimetype='application/pdf', as_attachment=False, download_name='report.pdf')
 
 if __name__ == '__main__':
+    if EXPECTED_TOKEN == "NA":
+        print("NO SECRET TOKEN SET. CANNOT CONTINUE")
+        exit()
     app.run(host='0.0.0.0', port=5000)
-    
+
